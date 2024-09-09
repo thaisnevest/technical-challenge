4c7e10b2dd696a982dcce42b156e146d3ef90c3f
thaisnevest <thaisneves@citi.org.br>
2024-09-08 23:01:02 -0300

    hotfix: added some explanatory coments

--- desafio-frontend/src/App.js
+++ desafio-frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import './App.css';
 
+// function to fetch API users based on page and limit provided
 const fetchUsers = async (page, limit) => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/users?_page=${page}&_limit=${limit}`);
   const data = await response.json();
@@ -9,6 +10,7 @@ const fetchUsers = async (page, limit) => {
 };
 
 function App() {
+  // states to perform desired functions
   const [users, setUsers] = useState([]);
   const [newName, setNewName] = useState("");
   const [newEmail, setNewEmail] = useState("");
@@ -19,12 +21,12 @@ function App() {
   const limit = 10;
 
   useEffect(() => {
-    // Carrega a primeira página de usuários
+    // function to load the first page of users
     const loadInitialUsers = async () => {
       setLoading(true);
       const data = await fetchUsers(page, limit);
       if (data.length < limit) {
-        setHasMore(false);
+        setHasMore(false); // if the amount of data is less than the limit, there is no more data to load
       }
       setUsers(data);
       setLoading(false);
@@ -32,8 +34,9 @@ function App() {
     loadInitialUsers();
   }, []);
 
+  // function to load more users when user scrolls down
   const loadMoreUsers = async () => {
-    if (loading || !hasMore) return; // Evita múltiplas requisições ou carregamento quando não há mais dados
+    if (loading || !hasMore) return;
     setLoading(true);
     const newPage = page + 1;
     const data = await fetchUsers(newPage, limit);
@@ -45,6 +48,7 @@ function App() {
     setLoading(false);
   };
 
+  // function to add a new user
   const addUser = () => {
     const name = newName.trim();
     const email = newEmail.trim();
@@ -68,6 +72,7 @@ function App() {
     }
   };
 
+  // function to update a user 
   const updateUser = id => {
     const user = users.find(user => user.id === id);
     fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
@@ -83,6 +88,7 @@ function App() {
       });
   };
 
+  // function to delete a user 
   const deleteUser = id => {
     fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
       method: "DELETE",
@@ -94,6 +100,7 @@ function App() {
       });
   };
 
+  // function to update email and website of a user
   const onChangeHandler = (id, key, value) => {
     setUsers(values => values.map(item =>
       item.id === id ? { ...item, [key]: value } : item
@@ -104,9 +111,9 @@ function App() {
     <div className="App">
       <InfiniteScroll
         dataLength={users.length}
-        next={loadMoreUsers}
+        next={loadMoreUsers} 
         hasMore={hasMore}
-        loader={<h4>Loading...</h4>}
+        loader={<h4>Loading...</h4>} 
         endMessage={<p>No more users to load</p>}
       >
         <table>
